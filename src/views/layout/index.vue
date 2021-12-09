cb78dbb0522d0a09ea173b65849c637e2a04d05a
JinTong <tong.jin@cdatatec.com>
2021-12-09 17:29:33 +0800

    修改用户名展示

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,287 +1,290 @@
 <template>
-	<div class="layout-wrapper">
-		<div class="layout-aside">
-			<div class="aside-header-wrapper" :class="{collapse:barCollapse}">
-				<router-link tag="div" to="/" class="logo-wrapper">
-					<img src="../../assets/images/logo.png" alt="">
-				</router-link>
+  <div class="layout-wrapper">
+    <div class="layout-aside">
+      <div class="aside-header-wrapper" :class="{ collapse: barCollapse }">
+        <router-link tag="div" to="/" class="logo-wrapper">
+          <img src="../../assets/images/logo.png" alt="" />
+        </router-link>
 
-				<div class="aside-content-wrapper clear-scroll-bar">
-					<SideBar :collapse="barCollapse"></SideBar>
-				</div>
-			</div>
+        <div class="aside-content-wrapper clear-scroll-bar">
+          <SideBar :collapse="barCollapse"></SideBar>
+        </div>
+      </div>
 
-			<div class="aside-footer-wrapper">
-				<div class="control-menu-collapse" @click="barCollapse = !barCollapse">
-					<i class="f-18" :class="barCollapse ? 'el-icon-s-unfold':'el-icon-s-fold'"></i>
-				</div>
-			</div>
-		</div>
+      <div class="aside-footer-wrapper">
+        <div class="control-menu-collapse" @click="barCollapse = !barCollapse">
+          <i class="f-18" :class="barCollapse ? 'el-icon-s-unfold' : 'el-icon-s-fold'"></i>
+        </div>
+      </div>
+    </div>
 
-		<div class="layout-main">
-			<div class="layout-header">
-				<div class="layout-navigate">
-					<TabCard :model="breadcrumbRouter"></TabCard>
-				</div>
+    <div class="layout-main">
+      <div class="layout-header">
+        <div class="layout-navigate">
+          <TabCard :model="breadcrumbRouter"></TabCard>
+        </div>
 
-				<el-popover placement="bottom-end" width="271">
-					<div class="header-info" slot="reference">
-						<el-avatar :size="24">
-							<img src="../../assets/images/logo.png" alt="">
-						</el-avatar>
+        <el-popover placement="bottom-end" width="271">
+          <div class="header-info" slot="reference">
+            <el-avatar :size="24">
+              <img src="../../assets/images/logo.png" alt="" />
+            </el-avatar>
 
-						<div class="info-name">用户名</div>
-					</div>
+            <div class="info-name">用户名</div>
+          </div>
 
-					<div class="user-info-popur-wrapper">
-						<div class="popur-info-wrapper">
-							<el-avatar :size="64">
-								<img src="../../assets/images/logo.png" alt="">
-							</el-avatar>
+          <div class="user-info-popur-wrapper">
+            <div class="popur-info-wrapper">
+              <el-avatar :size="64">
+                <img src="../../assets/images/logo.png" alt="" />
+              </el-avatar>
 
-							<div class="popur-info-main">
-								<div class="popur-info-name f-16">{{userInfo.name}}</div>
-								<div class="popur-info-manage-name">{{userInfo.loginAccount}}</div>
-							</div>
-						</div>
+              <div class="popur-info-main">
+                <div class="popur-info-name f-16">{{ userInfo.name }}</div>
+                <!-- <div class="popur-info-manage-name">{{ userInfo.loginAccount }}</div> -->
+              </div>
+            </div>
 
-						<div class="popur-info-item">
-							<span>角色</span>
-							<span>{{userInfo.roleName}}</span>
-						</div>
+            <div class="popur-info-item">
+              <span>角色</span>
+              <span>{{ userInfo.roleName }}</span>
+            </div>
 
-						<div class="popur-info-item">
-							<span>账号状态</span>
-							<span>{{userInfo.accountLock ? '':'未'}}锁定/{{userInfo.accountLock ? '已':'未'}}认证</span>
-						</div>
+            <!-- <div class="popur-info-item">
+              <span>账号状态</span>
+              <span>{{ userInfo.accountLock ? "" : "未" }}锁定/{{ userInfo.accountLock ? "已" : "未" }}认证</span>
+            </div>
 
-						<div class="popur-info-item">
-							<span>绑定邮箱</span>
-							<span>{{userInfo.mailbox}}</span>
-						</div>
+            <div class="popur-info-item">
+              <span>绑定邮箱</span>
+              <span>{{ userInfo.mailbox }}</span>
+            </div> -->
 
-						<el-button class="exit-btn" @click="$router.push('/login')" type="primary">退出登录</el-button>
-					</div>
-				</el-popover>
-			</div>
+            <el-button class="exit-btn" @click="$router.push('/login')" type="primary">退出登录</el-button>
+          </div>
+        </el-popover>
+      </div>
 
-			<main class="layout-content scrollbar">
-				<transition name="layout" mode="out-in">
-					<router-view></router-view>
-				</transition>
-			</main>
-		</div>
-	</div>
+      <main class="layout-content scrollbar">
+        <transition name="layout" mode="out-in">
+          <router-view></router-view>
+        </transition>
+      </main>
+    </div>
+  </div>
 </template>
 
 <script>
-import store from '@store';
-import { mapState, mapMutations } from 'vuex';
-import SideBar from '@components/side-bar';
-import TabCard from './components/tab-card.vue';
-import NProgress from 'nprogress';
-import { storages } from '@utils';
+import store from "@store";
+import { mapState, mapMutations } from "vuex";
+import SideBar from "@components/side-bar";
+import TabCard from "./components/tab-card.vue";
+import NProgress from "nprogress";
+import { storages } from "@utils";
 
 export default {
-	name: 'layout',
-	components: { SideBar, TabCard },
-	data() {
-		return {
-			barCollapse: false,
-			userInfo: {},
-		};
-	},
-	beforeRouteEnter(to, form, next) {
-		// console.log(to, form, '111');// 判断是否存在token，没有则跳至登录
-		if (store.state.__accessToken__) {
-			next();
-		} else {
-			NProgress.done();
-			next({ path: '/login', replace: true });
-		}
-	},
-	created() {
-		const userInfo = storages.get('userInfo') || {};
-		this.userInfo = Object.freeze(userInfo);
-	},
-	computed: {
-		...mapState(['breadcrumbRouter']),
-	},
-	methods: {
-		...mapMutations(['UPDATE_DEFAULT_ACTIVE']),
-	},
+  name: "layout",
+  components: { SideBar, TabCard },
+  data() {
+    return {
+      barCollapse: false,
+      userInfo: {
+        name: "webadmin",
+        roleName: "管理员",
+      },
+    };
+  },
+  beforeRouteEnter(to, form, next) {
+    // console.log(to, form, '111');// 判断是否存在token，没有则跳至登录
+    if (store.state.__accessToken__) {
+      next();
+    } else {
+      NProgress.done();
+      next({ path: "/login", replace: true });
+    }
+  },
+  created() {},
+  computed: {
+    ...mapState(["breadcrumbRouter"]),
+  },
+  methods: {
+    initUserinfo() {
+      const userInfo = storages.get("userInfo") || {};
+      this.userInfo = Object.freeze(userInfo);
+    },
+    ...mapMutations(["UPDATE_DEFAULT_ACTIVE"]),
+  },
 };
 </script>
 
-
 <style lang="scss" scoped>
 .layout-wrapper {
-	height: 100vh;
-	display: flex;
+  height: 100vh;
+  display: flex;
 
-	.layout-header {
-		height: 48px;
-		display: flex;
-		align-items: flex-end;
-		justify-content: space-between;
-		padding: 0 24px 0 10px;
-		background-color: #fff;
-	}
+  .layout-header {
+    height: 48px;
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    padding: 0 24px 0 10px;
+    background-color: #fff;
+  }
 
-	.layout-aside {
-		min-width: 0;
-		background-color: #001529;
-		display: flex;
-		height: 100%;
-		flex-direction: column;
-		justify-content: space-between;
-	}
+  .layout-aside {
+    min-width: 0;
+    background-color: #001529;
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    justify-content: space-between;
+  }
 
-	.layout-main {
-		flex: 1;
-		overflow: hidden;
-		background-color: #f5f9fd;
-		position: relative;
-		display: flex;
-		flex-direction: column;
-	}
+  .layout-main {
+    flex: 1;
+    overflow: hidden;
+    background-color: #f5f9fd;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+  }
 
-	.layout-navigate {
-		display: flex;
-		flex: 1;
-		margin-right: 20px;
+  .layout-navigate {
+    display: flex;
+    flex: 1;
+    margin-right: 20px;
 
-		// 超出时显示滚动条
-		overflow-x: auto;
-		overflow-y: hidden;
+    // 超出时显示滚动条
+    overflow-x: auto;
+    overflow-y: hidden;
 
-		&::-webkit-scrollbar {
-			height: 4px;
-		}
+    &::-webkit-scrollbar {
+      height: 4px;
+    }
 
-		/*定义滚动条轨道 内阴影+圆角*/
-		&::-webkit-scrollbar-track {
-			-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
-			box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
-			border-radius: 4px;
-			background-color: #fff;
-		}
+    /*定义滚动条轨道 内阴影+圆角*/
+    &::-webkit-scrollbar-track {
+      -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
+      box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
+      border-radius: 4px;
+      background-color: #fff;
+    }
 
-		/*定义滑块 内阴影+圆角*/
-		&::-webkit-scrollbar-thumb {
-			border-radius: 4px;
-			-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
-			box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
-			background-color: #999;
-		}
-	}
+    /*定义滑块 内阴影+圆角*/
+    &::-webkit-scrollbar-thumb {
+      border-radius: 4px;
+      -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
+      box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
+      background-color: #999;
+    }
+  }
 
-	.layout-content {
-		flex: 1;
-		overflow-x: hidden;
-		overflow-y: scroll;
-		background-color: #f5f9fd;
-	}
+  .layout-content {
+    flex: 1;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    background-color: #f5f9fd;
+  }
 }
 
 .aside-header-wrapper {
-	display: flex;
-	flex: 1;
-	min-height: 200px;
-	flex-direction: column;
+  display: flex;
+  flex: 1;
+  min-height: 200px;
+  flex-direction: column;
 
-	.logo-wrapper {
-		width: 56px;
-		height: 56px;
-		border-radius: 50%;
-		overflow: hidden;
-		margin: 30px auto;
-		cursor: pointer;
-	}
+  .logo-wrapper {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+    overflow: hidden;
+    margin: 30px auto;
+    cursor: pointer;
+  }
 
-	.aside-content-wrapper {
-		flex: 1;
-	}
+  .aside-content-wrapper {
+    flex: 1;
+  }
 }
 
 .header-info {
-	height: 48px;
-	display: flex;
-	align-items: center;
-	cursor: pointer;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
 
-	.info-name {
-		color: var(--font-color);
-		margin-left: 8px;
-	}
+  .info-name {
+    color: var(--font-color);
+    margin-left: 8px;
+  }
 }
 
 .control-menu-collapse {
-	height: 40px;
-	display: flex;
-	align-items: center;
-	padding-left: 23px;
-	cursor: pointer;
-	border-top: 1px solid rgba(255, 255, 255, 0.1);
-	i {
-		color: rgba(255, 255, 255, 0.64);
-	}
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 23px;
+  cursor: pointer;
+  border-top: 1px solid rgba(255, 255, 255, 0.1);
+  i {
+    color: rgba(255, 255, 255, 0.64);
+  }
 }
 
 .user-info-popur-wrapper {
-	.popur-info-wrapper {
-		display: flex;
-		align-items: center;
-		padding-bottom: 12px;
-		border-bottom: 1px solid #e4e7ed;
-		margin-bottom: 6px;
-	}
+  .popur-info-wrapper {
+    display: flex;
+    align-items: center;
+    padding-bottom: 12px;
+    border-bottom: 1px solid #e4e7ed;
+    margin-bottom: 6px;
+  }
 
-	.popur-info-main {
-		margin-left: 10px;
+  .popur-info-main {
+    margin-left: 10px;
 
-		.opur-info-name {
-			color: var(font-color-3);
-		}
+    .opur-info-name {
+      color: var(font-color-3);
+    }
 
-		.opur-info-manage-name {
-			color: var(font-color-a6);
-		}
-	}
+    .opur-info-manage-name {
+      color: var(font-color-a6);
+    }
+  }
 
-	.popur-info-item {
-		height: 36px;
-		line-height: 36px;
-		display: flex;
-		justify-content: space-between;
+  .popur-info-item {
+    height: 36px;
+    line-height: 36px;
+    display: flex;
+    justify-content: space-between;
 
-		span:first-child {
-			color: var(--font-color-1);
-		}
+    span:first-child {
+      color: var(--font-color-1);
+    }
 
-		span:last-child {
-			color: var(--font-color);
-		}
-	}
+    span:last-child {
+      color: var(--font-color);
+    }
+  }
 
-	.exit-btn {
-		width: 100%;
-		margin-top: 14px;
-	}
+  .exit-btn {
+    width: 100%;
+    margin-top: 14px;
+  }
 }
 </style>
 
 <style>
 .layout-enter,
 .layout-leave-to {
-	opacity: 0;
-	transform: translateY(30px);
+  opacity: 0;
+  transform: translateY(30px);
 }
 .layout-enter-active {
-	transition: all 0.2s;
+  transition: all 0.2s;
 }
 .layout-leave-active {
-	position: absolute;
-	transition: all 0.3s;
+  position: absolute;
+  transition: all 0.3s;
 }
 </style>

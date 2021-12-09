e32516f439fba697b5de27adbdda57bb3a651b8e
JinTong <tong.jin@cdatatec.com>
2021-12-09 18:28:19 +0800

    u

--- src/views/resource/equip-overview/index.vue
+++ src/views/resource/equip-overview/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="equip-overview-view-wrapper view-wrapper">
     <div class="inner-header-wrapper">
-      <div class="inner-container-title">设备框图</div>
+      <div class="inner-container-title equip-overview-title-wrapper">
+        设备框图
+        <div>自动刷新剩余时间：60秒 <el-button size="mini" type="primary">立即刷新</el-button></div>
+      </div>
       <div class="veneer-block-diagram-wrapper">
         <!-- 设备框图 -->
       </div>
@@ -9,13 +12,9 @@
 
     <div class="inner-container-wrapper banner-list">
       <div class="inner-container-title">系统信息</div>
-      <div></div>
+      <div class="system-info-wrapper"></div>
       <div class="inner-container-title">系统性能</div>
-      <div></div>
-    </div>
-
-    <div class="inner-pagination-wrapper">
-      <el-pagination background layout="prev, pager, next, jumper" :total="1000" />
+      <div class="system-performance-wrapper"></div>
     </div>
   </div>
 </template>
@@ -33,7 +32,11 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+.equip-overview-title-wrapper {
+  display: flex;
+  justify-content: space-between;
+}
 .veneer-block-diagram-wrapper {
   width: 80%;
   height: 200px;

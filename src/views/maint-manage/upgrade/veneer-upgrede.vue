12f2a8c09ab797053fc77c119e2c30403b4ad3a5
JinTong <tong.jin@cdatatec.com>
2021-12-10 14:52:51 +0800

    添加单板升级

--- src/views/maint-manage/upgrade/veneer-upgrede.vue
+++ src/views/maint-manage/upgrade/veneer-upgrede.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="system-upgrade-view-wrapper view-wrapper">
+  <div class="veneer-upgrede-view-wrapper view-wrapper">
     <div class="inner-container-wrapper banner-list">
-      <div class="inner-container-title">系统升级</div>
+      <div class="inner-container-title">需要升级的单板</div>
+      <div>
+        <el-checkbox>单板#2</el-checkbox>
+        <el-checkbox>单板#4</el-checkbox>
+        <el-checkbox>单板#6</el-checkbox>
+        <el-checkbox>单板#8</el-checkbox>
+      </div>
+      <div class="inner-container-title">升级单板固件</div>
       <el-upload class="upload-demo" accept=".bin" drag action="" :auto-upload="false" :file-list="fileList">
         <i class="el-icon-upload"></i>
         <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
         <div class="el-upload__tip" slot="tip">只能上传bin文件</div>
       </el-upload>
-      <el-button class="system-upgrade-submit" type="primary">升级</el-button>
+      <el-button class="veneer-upgrede-submit" type="primary">升级</el-button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "system-upgrade",
+  name: "veneer-upgrede",
   data() {
     return {
       formData: {},
@@ -31,7 +38,7 @@ export default {
 </script>
 
 <style scoped lang="css">
-.system-upgrade-submit {
+.veneer-upgrede-submit {
   margin-top: 20px;
   width: 140px;
 }

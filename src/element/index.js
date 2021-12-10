3e997264ecf8ff3949833c529b8ec877c2e144e0
JinTong <tong.jin@cdatatec.com>
2021-12-10 15:37:02 +0800

    删除无用文件

--- src/element/index.js
+++ src/element/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import {
-  Menu, MenuItem, Submenu, Table, TableColumn, Form, FormItem, Radio, Select, Input, InputNumber,
-  DatePicker, Option, Col, Row, Pagination, Popover, Button, Avatar, Dialog, Progress,
-  Carousel, CarouselItem, Upload, MessageBox, Message, Notification, Loading, Switch, Checkbox
+  Menu, MenuItem, Submenu, Table, TableColumn, Form, FormItem, Select, Input, DatePicker, Option, Col, Row,
+  Pagination, Button, Avatar, Dialog, Upload, MessageBox, Message, Notification, Loading, Checkbox
 } from 'element-ui';
 
 export let load = null;
@@ -28,7 +27,6 @@ Vue
   .use(TableColumn)
   .use(Form)
   .use(FormItem)
-  .use(Radio)
   .use(Select)
   .use(Option)
   .use(Col)
@@ -37,13 +35,7 @@ Vue
   .use(Avatar)
   .use(Pagination)
   .use(Input)
-  .use(InputNumber)
   .use(DatePicker)
   .use(Dialog)
-  .use(Progress)
-  .use(Carousel)
-  .use(CarouselItem)
   .use(Upload)
-  .use(Popover)
-  .use(Switch)
   .use(Checkbox)

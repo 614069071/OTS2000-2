017ce0f85752da53cec76c9a95f68a460233023c
JinTong <tong.jin@cdatatec.com>
2021-12-17 11:30:10 +0800

    修改snmp

--- src/views/network-manage/snmp-config.vue
+++ src/views/network-manage/snmp-config.vue
@@ -14,30 +14,36 @@
           <div class="system-info-item"><span>SNMP读共同体</span><span>pulic</span></div>
           <div class="system-info-item"><span>SNMP写共同体</span><span>private</span></div>
         </div>
-        <el-button class="change-global-snmp-btn" type="primary">修改</el-button>
+
+        <div class="change-global-snmp-btn">
+          <button class="def-btn">保存配置</button>
+        </div>
       </div>
 
       <div class="inner-container-title">SNMP Trap配置</div>
 
       <el-table ref="multipleTable" :data="dataTable" tooltip-effect="dark" style="width: 100%">
-        <el-table-column type="index" label="序号" width="50"> </el-table-column>
-        <el-table-column prop="name2" label="目标主机名称"></el-table-column>
+        <el-table-column type="index" label="序号" width="60"> </el-table-column>
+        <el-table-column prop="name1" label="目标主机名称"></el-table-column>
+        <el-table-column prop="name2" label="IP地址"></el-table-column>
         <el-table-column prop="name3" label="UDP通信端口"></el-table-column>
         <el-table-column prop="name4" label="状态"></el-table-column>
         <el-table-column prop="name5" label="操作" width="160">
           <template slot-scope="scope">
-            <el-button type="warning" size="mini">修改</el-button>
-            <el-button type="danger" size="mini">删除</el-button>
+            <button class="def-btn">修改</button>
+            <button class="def-btn">删除</button>
           </template>
         </el-table-column>
       </el-table>
+      <div class="add-trap-config-wrapper">
+        <input class="def-input" type="text" placeholder="主机名称" />
+        <input class="def-input" type="text" placeholder="IP地址" />
+        <input class="def-input" type="text" placeholder="UDP通信端口" />
 
-      <div class="inner-pagination-wrapper inner-pagination-colle">
-        <div class="pagination-btns-wrapper">
-          <el-button size="mini" type="primary">增加Trap</el-button>
-        </div>
-        <el-pagination background layout="prev,pager,next,jumper" :total="1000" />
+        <button class="def-btn">启用</button>
       </div>
+
+      <div class="add-trap-wrapper"><button class="def-btn">增加Trap</button></div>
     </div>
   </div>
 </template>
@@ -50,10 +56,10 @@ export default {
       dataForm: {},
       dataTable: [
         {
-          name2: "host 238",
-          name3: "192.168.1.212",
-          name4: "9162",
-          name5: "启用",
+          name1: "host222",
+          name2: "192.168.1.121",
+          name3: "9162",
+          name4: "启用",
         },
       ],
     };
@@ -72,30 +78,27 @@ export default {
 }
 
 .global-config-snmp-wrapper {
-  display: flex;
+  padding-top: 10px;
 }
 
 .global-info-wrapper {
-  width: 70%;
-  display: flex;
-  flex-wrap: wrap;
   border-top: 1px solid grey;
   border-left: 1px solid grey;
   .system-info-item {
-    width: 50%;
     display: flex;
     align-items: center;
 
     span {
-      height: 48px;
+      height: 50px;
+      line-height: 50px;
       border-right: 1px solid grey;
       border-bottom: 1px solid grey;
-      line-height: 48px;
+      color: #a7a7a7;
     }
 
     span:first-child {
       background-color: #f4f7fa;
-      width: 120px;
+      width: 30%;
       text-align: center;
     }
     span:last-child {
@@ -106,6 +109,20 @@ export default {
 }
 
 .change-global-snmp-btn {
-  margin-left: 20px;
+  margin: 13px 0 40px 0;
+  text-align: right;
+}
+
+.add-trap-config-wrapper {
+  white-space: nowrap;
+  padding: 10px 0 0 60px;
+  input {
+    margin-right: 20px;
+  }
+}
+
+.add-trap-wrapper {
+  text-align: right;
+  margin-top: 20px;
 }
 </style>

db2631e345cb3c858bd99fbd2ed565e8d6b5166e
JinTong <tong.jin@cdatatec.com>
2021-12-20 18:09:11 +0800

    添加单板nmu

--- src/views/equip-manage/veneer-list.vue
+++ src/views/equip-manage/veneer-list.vue
@@ -23,6 +23,44 @@
         </el-table-column>
       </el-table>
     </div>
+
+    <el-dialog title="单板详情" :visible.sync="dialogVisible" width="70%">
+      <div class="veneer-header-wrapper">我是图</div>
+      <div class="veneer-main-wrapper">
+        <div class="veneer-channel-item"><span class="border">硬件版本</span><span class="border"></span></div>
+        <div class="veneer-channel-item"><span class="border">软件件版本</span><span class="border"></span></div>
+        <div class="veneer-channel-item"><span class="border">协议版本</span><span class="border"></span></div>
+        <div class="veneer-channel-item"><span class="border">生产日期</span><span class="border"></span></div>
+        <div class="veneer-channel-item"><span class="border">序列号</span><span class="border"></span></div>
+        <div class="veneer-channel-item"><span class="border">运行时间</span><span class="border"></span></div>
+        <div class="veneer-channel-item"><span class="border">设备类型</span><span class="border"></span></div>
+        <div class="veneer-channel-item"><span class="border">状态</span><span class="border"></span></div>
+        <div class="veneer-channel-item"><span class="border">信息描述</span><span class="border"></span></div>
+      </div>
+
+      <div class="venner-change-btns">
+        <button class="def-btn">刷新</button>
+        <button class="def-btn">应用</button>
+      </div>
+
+      <div class="venner-change-info-wrapper">
+        <div class="venner-change-info-item">
+          <span class="border">SFP端口</span>
+          <span class="border">SFP1</span>
+          <span class="border">SFP2</span>
+          <span class="border">SFP3</span>
+        </div>
+      </div>
+
+      <div class="venner-change-info-wrapper">
+        <div class="venner-change-info-item">
+          <span class="border">ETH端口</span>
+          <span class="border">ETH1</span>
+          <span class="border">ETH2</span>
+          <span class="border">ETH3</span>
+        </div>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -42,17 +80,20 @@ export default {
         name6: "4.0.1",
         name7: "2121-5-20",
       }),
+      dialogVisible: false,
     };
   },
   methods: {
     lookDetail() {
       console.log("查看详情");
+
+      this.dialogVisible = true;
     },
   },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .veneer-block-diagram-wrapper {
   width: 80%;
   height: 200px;
@@ -67,5 +108,59 @@ export default {
 
 .veneer-list {
   margin-top: 40px;
+}
+
+.veneer-header-wrapper {
+  height: 77px;
+  border: 1px solid red;
+}
+
+.border {
+  height: 36px;
+  line-height: 36px;
+  border-right: 1px solid #e8e8e8;
+  border-bottom: 1px solid #e8e8e8;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.veneer-main-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+  border-top: 1px solid #e8e8e8;
+  border-left: 1px solid #e8e8e8;
+
+  .veneer-channel-item {
+    width: 33.33%;
+    display: flex;
+  }
+
+  .veneer-channel-item span:first-child {
+    width: 40%;
+    text-align: center;
+  }
+
+  .veneer-channel-item span:last-child {
+    flex: 1;
+    padding-left: 10px;
+  }
+}
+.venner-change-btns {
+  text-align: right;
+  padding: 10px 0;
+}
+
+.venner-change-info-wrapper {
+  margin-top: 40px;
+  border-top: 1px solid #e8e8e8;
+  border-left: 1px solid #e8e8e8;
+  .venner-change-info-item {
+    display: flex;
+
+    span {
+      width: 25%;
+    }
+  }
 }
 </style>

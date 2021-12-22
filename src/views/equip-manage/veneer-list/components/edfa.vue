9ba260367f93d1fe7be0615432ff7df1d93f1bf7
Jint <614069071@qq.com>
2021-12-22 15:15:02 +0800

    添加edfa详情

--- src/views/equip-manage/veneer-list/components/edfa.vue
+++ src/views/equip-manage/veneer-list/components/edfa.vue
@@ -17,23 +17,138 @@
       <button class="def-btn">刷新</button>
       <button class="def-btn">应用</button>
     </div>
-
-    <div class="venner-change-info-wrapper">
-      <div class="venner-change-info-item">
-        <span class="border">SFP端口</span>
-        <span class="border">SFP1</span>
-        <span class="border">SFP2</span>
-        <span class="border">SFP3</span>
-      </div>
+    <!-- 状态信息 -->
+    <div class="veneer-main-wrapper">
+      <div class="veneer-channel-item"><span class="border">输入光功率（dBm）</span><span class="border"></span></div>
+      <div class="veneer-channel-item"><span class="border">输出光功率（dBm）</span><span class="border"></span></div>
+      <div class="veneer-channel-item"><span class="border">TEC制冷电流（mA）</span><span class="border"></span></div>
+      <div class="veneer-channel-item"><span class="border">模块温度（℃）</span><span class="border"></span></div>
+      <div class="veneer-channel-item"><span class="border">PUMP温度（℃）</span><span class="border"></span></div>
+      <div class="veneer-channel-item"><span class="border">PUMP电流（mA）</span><span class="border"></span></div>
+      <div class="veneer-channel-item"><span class="border">输入功率告警</span><span class="border"></span></div>
+      <div class="veneer-channel-item"><span class="border">输出功率告警</span><span class="border"></span></div>
+      <div class="veneer-channel-item"><span class="border">PUMP电流告警</span><span class="border"></span></div>
+      <div class="veneer-channel-item"><span class="border">模块温度告警</span><span class="border"></span></div>
+      <div class="veneer-channel-item"><span class="border">PUMP温度告警</span><span class="border"></span></div>
+      <div class="veneer-channel-item"><span class="border"></span><span class="border"></span></div>
     </div>
 
-    <div class="venner-change-info-wrapper">
-      <div class="venner-change-info-item">
-        <span class="border">ETH端口</span>
-        <span class="border">ETH1</span>
-        <span class="border">ETH2</span>
-        <span class="border">ETH3</span>
+    <div class="veneer-edfa-setting-wrapper">
+      <div class="veneer-edfa-item">
+        <span>PUMP关断</span>
+        <span>
+          <select size="mini" v-model="changeForm.name1">
+            <option value="1">打开</option>
+            <option value="2">关闭</option>
+          </select>
+        </span>
       </div>
+      <div class="veneer-edfa-item">
+        <span>工作模式</span>
+        <span class="edfa-after">
+          <select size="mini" v-model="changeForm.name2">
+            <option value="1">APC</option>
+            <option value="2">AGC</option>
+            <option value="2">ACC</option>
+            <option value="3">自定义</option>
+          </select>
+          <input class="def-input edfa-input" type="text" />
+        </span>
+      </div>
+      <div class="veneer-edfa-item">
+        <span>输入光功率门限（dBm）</span>
+        <span class="edfa-after">
+          <select size="mini" v-model="changeForm.name2">
+            <option value="1">-20</option>
+            <option value="2">-26</option>
+            <option value="2">-31</option>
+            <option value="3">自定义</option>
+          </select>
+          <input class="def-input edfa-input" type="text" />
+        </span>
+      </div>
+      <div class="veneer-edfa-item">
+        <span>输出光功率门限（dBm）</span>
+        <span class="edfa-after">
+          <select size="mini" v-model="changeForm.name2">
+            <option value="1">-2</option>
+            <option value="2">-5</option>
+            <option value="2">-3</option>
+            <option value="3">自定义</option>
+          </select>
+          <input class="def-input edfa-input" type="text" />
+        </span>
+      </div>
+      <div class="veneer-edfa-item">
+        <span>PUMP电流门限（mA）</span>
+        <span class="edfa-after">
+          <select v-model="changeForm.name2">
+            <option value="1">600</option>
+            <option value="2">800</option>
+            <option value="3">自定义</option>
+          </select>
+          <input class="def-input edfa-input" type="text" />
+        </span>
+      </div>
+      <div class="veneer-edfa-item">
+        <span>PUMP关断电流（mA）</span>
+        <span class="edfa-after">
+          <select v-model="changeForm.name2">
+            <option value="1">50</option>
+            <option value="3">自定义</option>
+          </select>
+          <input class="def-input edfa-input" type="text" />
+        </span>
+      </div>
+      <div class="veneer-edfa-item">
+        <span>模块温度低门限（℃）</span>
+        <span class="edfa-after">
+          <select v-model="changeForm.name2">
+            <option value="1">-30</option>
+            <option value="1">-40</option>
+            <option value="1">-50</option>
+            <option value="3">自定义</option>
+          </select>
+          <input class="def-input edfa-input" type="text" />
+        </span>
+      </div>
+      <div class="veneer-edfa-item">
+        <span>PUMP温度低门限（℃）</span>
+        <span class="edfa-after">
+          <select v-model="changeForm.name2">
+            <option value="1">20</option>
+            <option value="3">自定义</option>
+          </select>
+          <input class="def-input edfa-input" type="text" />
+        </span>
+      </div>
+      <div class="veneer-edfa-item">
+        <span>模块温度高门限</span>
+        <span class="edfa-after">
+          <select v-model="changeForm.name2">
+            <option value="1">50</option>
+            <option value="2">60</option>
+            <option value="3">70</option>
+            <option value="4">自定义</option>
+          </select>
+          <input class="def-input edfa-input" type="text" />
+        </span>
+      </div>
+      <div class="veneer-edfa-item">
+        <span>PUMP温度高门限（℃）</span>
+        <span class="edfa-after">
+          <select v-model="changeForm.name2">
+            <option value="1">30</option>
+            <option value="4">自定义</option>
+          </select>
+          <input class="def-input edfa-input" type="text" />
+        </span>
+      </div>
+    </div>
+    <div class="venner-change-btns">
+      <button class="def-btn">刷新</button>
+      <button class="def-btn">应用</button>
+      <button class="def-btn">恢复默认</button>
     </div>
   </div>
 </template>
@@ -42,7 +157,12 @@
 export default {
   name: "edfa",
   data() {
-    return {};
+    return {
+      changeForm: {
+        name1: "",
+        name2: "1",
+      },
+    };
   },
   created() {
     console.log("created");
@@ -96,6 +216,32 @@ export default {
     span {
       width: 25%;
     }
+  }
+}
+
+.veneer-edfa-setting-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  .veneer-edfa-item {
+    width: 50%;
+    height: 30px;
+    margin-top: 7px;
+    display: flex;
+    align-items: center;
+
+    .edfa-input {
+      width: 80px;
+      height: 28px;
+      margin-left: 10px;
+    }
+  }
+
+  .veneer-edfa-item > span:first-child {
+    width: 65%;
+    text-align: center;
+  }
+  .veneer-edfa-item > span:last-child {
+    display: flex;
   }
 }
 </style>

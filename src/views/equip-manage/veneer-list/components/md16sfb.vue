ef5e044029830e50cb3c3e1dfee6beed629ad699
Jint <614069071@qq.com>
2021-12-23 13:49:24 +0800

    添加单板Md16sfb

---
+++ src/views/equip-manage/veneer-list/components/md16sfb.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="md16sfb-cps-wrapper">
+    <div class="veneer-header-wrapper">dcm</div>
+    <table class="veneer-table veneer-title-table" border="1">
+      <tr>
+        <td>硬件版本</td>
+        <td>{{ veneerTitleData.h_rev }}</td>
+        <td>软件版本</td>
+        <td>{{ veneerTitleData.s_rev }}</td>
+        <td>协议版本</td>
+        <td>{{ veneerTitleData.mfgdate }}</td>
+      </tr>
+      <tr>
+        <td>生产日期</td>
+        <td>{{ veneerTitleData.mfgdate }}</td>
+        <td>序列号</td>
+        <td>{{ veneerTitleData.serialnum }}</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>设备类型</td>
+        <td>{{ veneerTitleData.device_type }}</td>
+        <td>状态</td>
+        <td>{{ veneerTitleData.status }}</td>
+        <td>信息描述</td>
+        <td>{{ veneerTitleData.desc }}</td>
+      </tr>
+    </table>
+
+    <div class="venner-change-btns">
+      <button class="def-btn">刷新</button>
+      <button class="def-btn">应用</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "md16sfb",
+  props: ["info"],
+  data() {
+    return {
+      veneerTitleData: {},
+    };
+  },
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>

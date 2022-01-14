6250d8293a90083e50cbb52ed16b50e63c6dee49
Jint <614069071@qq.com>
2022-01-14 10:56:58 +0800

    修改nmu

--- src/views/equip-manage/veneer-list/veneer-type/nmu.vue
+++ src/views/equip-manage/veneer-list/veneer-type/nmu.vue
@@ -2,33 +2,33 @@
   <div class="num-cps-wrapper cps-wrapper">
     <div class="veneer-header-wrapper">
       <!-- <div class="veneer-inner-wrapper"></div> -->
-      <NmuPic :data="veneerInfoData"></NmuPic>
+      <NmuPic :data="infoData"></NmuPic>
     </div>
     <div class="veneer-table-title">
       <table class="veneer-table" border="1">
         <tr>
           <td>硬件版本</td>
-          <td>{{ `${veneerTitleData.h_rev ? "V" + veneerTitleData.h_rev : ""}` }}</td>
+          <td>{{ `${titeData.h_rev ? "V" + titeData.h_rev : ""}` }}</td>
           <td>软件版本</td>
-          <td>{{ `${veneerTitleData.s_rev ? "V" + veneerTitleData.s_rev : ""}` }}</td>
+          <td>{{ `${titeData.s_rev ? "V" + titeData.s_rev : ""}` }}</td>
           <td>协议版本</td>
-          <td>{{ `${veneerTitleData.p_rev ? "V" + veneerTitleData.p_rev : ""}` }}</td>
+          <td>{{ `${titeData.p_rev ? "V" + titeData.p_rev : ""}` }}</td>
         </tr>
         <tr>
           <td>生产日期</td>
           <td>
-            <el-date-picker v-if="$store.state.iSuper" v-model="veneerTitleData.mfgdate" size="mini" type="date" value-format="yyyy-MM-dd" />
+            <el-date-picker v-if="$store.state.iSuper" v-model="titeData.mfgdate" size="mini" type="date" value-format="yyyy-MM-dd" />
 
             <template v-else>
-              {{ veneerTitleData.mfgdate }}
+              {{ titeData.mfgdate }}
             </template>
           </td>
           <td>序列号</td>
           <td>
-            <input class="def-input" v-if="$store.state.iSuper" type="text" v-model="veneerTitleData.sn" />
+            <input class="def-input" v-if="$store.state.iSuper" type="text" v-model="titeData.sn" />
 
             <template v-else>
-              {{ veneerTitleData.sn }}
+              {{ titeData.sn }}
             </template>
           </td>
           <td></td>
@@ -36,12 +36,12 @@
         </tr>
         <tr>
           <td>设备类型</td>
-          <td>{{ veneerTitleData.device_type }}</td>
+          <td>{{ titeData.device_type }}</td>
           <td>状态</td>
-          <td>{{ veneerTitleData.status ? "在位" : "脱位" }}</td>
+          <td>{{ titeData.status ? "在位" : "脱位" }}</td>
           <td>信息描述</td>
           <td>
-            <input class="def-input" type="text" v-model="veneerTitleData.desc" />
+            <input class="def-input" type="text" v-model="titeData.desc" />
           </td>
         </tr>
       </table>
@@ -65,63 +65,63 @@
 
         <tr>
           <td>在位状态</td>
-          <td>{{ veneerInfoData.sfp1.online_status ? "在位" : "脱位" }}</td>
-          <td>{{ veneerInfoData.sfp2.online_status ? "在位" : "脱位" }}</td>
-          <td>{{ veneerInfoData.sfp3.online_status ? "在位" : "脱位" }}</td>
+          <td>{{ infoData.sfp1.online_status ? "在位" : "脱位" }}</td>
+          <td>{{ infoData.sfp2.online_status ? "在位" : "脱位" }}</td>
+          <td>{{ infoData.sfp3.online_status ? "在位" : "脱位" }}</td>
         </tr>
         <tr>
           <td>LINK状态</td>
-          <td>{{ veneerInfoData.sfp1.online_status ? (veneerInfoData.sfp1.link_status ? "UP" : "DOWN") : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp2.online_status ? (veneerInfoData.sfp2.link_status ? "UP" : "DOWN") : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp3.online_status ? (veneerInfoData.sfp3.link_status ? "UP" : "DOWN") : "NA" }}</td>
+          <td>{{ infoData.sfp1.online_status ? (infoData.sfp1.link_status ? "UP" : "DOWN") : "NA" }}</td>
+          <td>{{ infoData.sfp2.online_status ? (infoData.sfp2.link_status ? "UP" : "DOWN") : "NA" }}</td>
+          <td>{{ infoData.sfp3.online_status ? (infoData.sfp3.link_status ? "UP" : "DOWN") : "NA" }}</td>
         </tr>
         <tr>
           <td>速率（Mbps）</td>
-          <td>{{ veneerInfoData.sfp1.online_status ? veneerInfoData.sfp1.speed_Gbps : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp2.online_status ? veneerInfoData.sfp2.speed_Gbps : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp3.online_status ? veneerInfoData.sfp3.speed_Gbps : "NA" }}</td>
+          <td>{{ infoData.sfp1.online_status ? infoData.sfp1.speed_Gbps : "NA" }}</td>
+          <td>{{ infoData.sfp2.online_status ? infoData.sfp2.speed_Gbps : "NA" }}</td>
+          <td>{{ infoData.sfp3.online_status ? infoData.sfp3.speed_Gbps : "NA" }}</td>
         </tr>
         <tr>
           <td>波长（nm）</td>
-          <td>{{ veneerInfoData.sfp1.online_status ? veneerInfoData.sfp1.wave_len : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp2.online_status ? veneerInfoData.sfp2.wave_len : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp3.online_status ? veneerInfoData.sfp3.wave_len : "NA" }}</td>
+          <td>{{ infoData.sfp1.online_status ? infoData.sfp1.wave_len : "NA" }}</td>
+          <td>{{ infoData.sfp2.online_status ? infoData.sfp2.wave_len : "NA" }}</td>
+          <td>{{ infoData.sfp3.online_status ? infoData.sfp3.wave_len : "NA" }}</td>
         </tr>
         <tr>
           <td>距离（km）</td>
-          <td>{{ veneerInfoData.sfp1.online_status ? veneerInfoData.sfp1.tx_distanst : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp2.online_status ? veneerInfoData.sfp2.tx_distanst : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp3.online_status ? veneerInfoData.sfp3.tx_distanst : "NA" }}</td>
+          <td>{{ infoData.sfp1.online_status ? infoData.sfp1.tx_distanst : "NA" }}</td>
+          <td>{{ infoData.sfp2.online_status ? infoData.sfp2.tx_distanst : "NA" }}</td>
+          <td>{{ infoData.sfp3.online_status ? infoData.sfp3.tx_distanst : "NA" }}</td>
         </tr>
         <tr>
           <td>发送光功率（dBm）</td>
-          <td>{{ veneerInfoData.sfp1.online_status ? veneerInfoData.sfp1.launch_power : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp2.online_status ? veneerInfoData.sfp2.launch_power : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp3.online_status ? veneerInfoData.sfp3.launch_power : "NA" }}</td>
+          <td>{{ infoData.sfp1.online_status ? infoData.sfp1.launch_power : "NA" }}</td>
+          <td>{{ infoData.sfp2.online_status ? infoData.sfp2.launch_power : "NA" }}</td>
+          <td>{{ infoData.sfp3.online_status ? infoData.sfp3.launch_power : "NA" }}</td>
         </tr>
         <tr>
           <td>接受光功率（dBm）</td>
-          <td>{{ veneerInfoData.sfp1.online_status ? veneerInfoData.sfp1.rcv_power : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp2.online_status ? veneerInfoData.sfp2.rcv_power : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp3.online_status ? veneerInfoData.sfp3.rcv_power : "NA" }}</td>
+          <td>{{ infoData.sfp1.online_status ? infoData.sfp1.rcv_power : "NA" }}</td>
+          <td>{{ infoData.sfp2.online_status ? infoData.sfp2.rcv_power : "NA" }}</td>
+          <td>{{ infoData.sfp3.online_status ? infoData.sfp3.rcv_power : "NA" }}</td>
         </tr>
         <tr>
           <td>电压（V）</td>
-          <td>{{ veneerInfoData.sfp1.online_status ? veneerInfoData.sfp1.voltage : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp2.online_status ? veneerInfoData.sfp2.voltage : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp3.online_status ? veneerInfoData.sfp3.voltage : "NA" }}</td>
+          <td>{{ infoData.sfp1.online_status ? infoData.sfp1.voltage : "NA" }}</td>
+          <td>{{ infoData.sfp2.online_status ? infoData.sfp2.voltage : "NA" }}</td>
+          <td>{{ infoData.sfp3.online_status ? infoData.sfp3.voltage : "NA" }}</td>
         </tr>
         <tr>
           <td>电流（mA）</td>
-          <td>{{ veneerInfoData.sfp1.online_status ? veneerInfoData.sfp1.current : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp2.online_status ? veneerInfoData.sfp2.current : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp3.online_status ? veneerInfoData.sfp3.current : "NA" }}</td>
+          <td>{{ infoData.sfp1.online_status ? infoData.sfp1.current : "NA" }}</td>
+          <td>{{ infoData.sfp2.online_status ? infoData.sfp2.current : "NA" }}</td>
+          <td>{{ infoData.sfp3.online_status ? infoData.sfp3.current : "NA" }}</td>
         </tr>
         <tr>
           <td>温度（℃）</td>
-          <td>{{ veneerInfoData.sfp1.online_status ? veneerInfoData.sfp1.temp : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp2.online_status ? veneerInfoData.sfp2.temp : "NA" }}</td>
-          <td>{{ veneerInfoData.sfp3.online_status ? veneerInfoData.sfp3.temp : "NA" }}</td>
+          <td>{{ infoData.sfp1.online_status ? infoData.sfp1.temp : "NA" }}</td>
+          <td>{{ infoData.sfp2.online_status ? infoData.sfp2.temp : "NA" }}</td>
+          <td>{{ infoData.sfp3.online_status ? infoData.sfp3.temp : "NA" }}</td>
         </tr>
       </table>
 
@@ -134,21 +134,21 @@
         </tr>
         <tr>
           <td>LINK状态</td>
-          <td>{{ veneerInfoData.eth1.eth_status ? "UP" : "DOWN" }}</td>
-          <td>{{ veneerInfoData.eth2.eth_status ? "UP" : "DOWN" }}</td>
-          <td>{{ veneerInfoData.eth3.eth_status ? "UP" : "DOWN" }}</td>
+          <td>{{ infoData.eth1.eth_status ? "UP" : "DOWN" }}</td>
+          <td>{{ infoData.eth2.eth_status ? "UP" : "DOWN" }}</td>
+          <td>{{ infoData.eth3.eth_status ? "UP" : "DOWN" }}</td>
         </tr>
         <tr>
           <td>速率（Mbps）</td>
-          <td>{{ veneerInfoData.eth1.eth_status ? veneerInfoData.eth1.ethspeed : "NA" }}</td>
-          <td>{{ veneerInfoData.eth2.eth_status ? veneerInfoData.eth2.ethspeed : "NA" }}</td>
-          <td>{{ veneerInfoData.eth3.eth_status ? veneerInfoData.eth3.ethspeed : "NA" }}</td>
+          <td>{{ infoData.eth1.eth_status ? infoData.eth1.ethspeed : "NA" }}</td>
+          <td>{{ infoData.eth2.eth_status ? infoData.eth2.ethspeed : "NA" }}</td>
+          <td>{{ infoData.eth3.eth_status ? infoData.eth3.ethspeed : "NA" }}</td>
         </tr>
         <tr>
           <td>双工</td>
-          <td>{{ veneerInfoData.eth1.eth_status ? (veneerInfoData.eth1.full_status ? "全双工" : "半双工") : "NA" }}</td>
-          <td>{{ veneerInfoData.eth2.eth_status ? (veneerInfoData.eth2.full_status ? "全双工" : "半双工") : "NA" }}</td>
-          <td>{{ veneerInfoData.eth3.eth_status ? (veneerInfoData.eth3.full_status ? "全双工" : "半双工") : "NA" }}</td>
+          <td>{{ infoData.eth1.eth_status ? (infoData.eth1.full_status ? "全双工" : "半双工") : "NA" }}</td>
+          <td>{{ infoData.eth2.eth_status ? (infoData.eth2.full_status ? "全双工" : "半双工") : "NA" }}</td>
+          <td>{{ infoData.eth3.eth_status ? (infoData.eth3.full_status ? "全双工" : "半双工") : "NA" }}</td>
         </tr>
       </table>
     </div>
@@ -160,15 +160,16 @@
 </template>
 
 <script>
+import mixins from "@/utils/mixins";
 import NmuPic from "@/components/veneer/nmu.vue";
 
 export default {
   name: "nmu",
-  props: ["info", "visible"],
   components: { NmuPic },
+  mixins: [mixins],
   data() {
     return {
-      veneerTitleData: {
+      titeData: {
         boardname: "",
         type: "",
         h_rev: "",
@@ -181,7 +182,7 @@ export default {
         status: 0,
         desc: "",
       },
-      veneerInfoData: {
+      infoData: {
         sfp1: {
           online_status: 0,
           link_status: 0,
@@ -234,102 +235,7 @@ export default {
           ethspeed: 0,
         },
       },
-      refreshTitleDisabled: false,
-      setTilteDisabled: false,
-      refreshInfoDisabled: false,
     };
-  },
-  created() {},
-  mounted() {
-    this.getVeneerDetail();
-  },
-  watch: {
-    visible(n) {
-      if (!n) return;
-      this.getVeneerDetail();
-    },
-  },
-  methods: {
-    getVeneerTitle() {
-      const { boardname, slot } = this.info;
-      const data = { otn2000: { type: "get_title", boardname, slot } };
-
-      return this.$http.post(data);
-    },
-    getVeneerInfo() {
-      const { boardname, slot } = this.info;
-      const data = { otn2000: { type: "get_info", boardname, slot } };
-
-      return this.$http.post(data);
-    },
-    getVeneerDetail() {
-      Promise.all([this.getVeneerTitle(), this.getVeneerInfo()])
-        .then((res) => {
-          const [resTitle, resInfo] = res;
-
-          this.veneerTitleData = resTitle.otn2000_ack;
-          this.veneerInfoData = resInfo.otn2000_ack || {};
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-    refreshTitle() {
-      this.refreshTitleDisabled = true;
-
-      this.getVeneerTitle()
-        .then((res) => {
-          console.log(res);
-          this.veneerTitleData = res.otn2000_ack;
-          this.$message("成功");
-          this.refreshTitleDisabled = false;
-        })
-        .catch((err) => {
-          console.log(err);
-          this.$message("失败");
-          this.refreshTitleDisabled = false;
-        });
-    },
-    setTilte() {
-      const { mfgdate, sn, desc } = this.veneerTitleData;
-      const { boardname, slot } = this.info;
-      const iSuperData = this.$store.state.iSuper ? { mfgdate, sn } : {};
-      const data = { otn2000: { type: "post_title", boardname, desc, slot, ...iSuperData } };
-      this.setTilteDisabled = true;
-
-      this.$http
-        .post(data)
-        .then((res) => {
-          console.log("setTilte", res);
-          this.$message("成功");
-          this.setTilteDisabled = false;
-        })
-        .catch((err) => {
-          console.log(err);
-          this.veneerTitleData.desc = "";
-          this.$message("失败");
-
-          this.setTilteDisabled = false;
-        });
-    },
-    refreshInfo() {
-      this.refreshInfoDisabled = true;
-
-      this.getVeneerInfo()
-        .then((res) => {
-          console.log(res);
-          this.veneerInfoData = res.otn2000_ack;
-          this.$message("成功");
-
-          this.refreshInfoDisabled = false;
-        })
-        .catch((err) => {
-          console.log(err);
-          this.$message("失败");
-
-          this.refreshInfoDisabled = false;
-        });
-    },
   },
 };
 </script>

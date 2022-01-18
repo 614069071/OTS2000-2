bb48817b84a3c975bf4ab1d64915d0ff29471591
Jint <614069071@qq.com>
2022-01-18 17:32:46 +0800

    u

--- src/components/structure/index.vue
+++ src/components/structure/index.vue
@@ -2,24 +2,37 @@
   <div class="structure-wrapper">
     <div class="structure-inner-wrapper">
       <div class="structure-item-wrapper" v-for="(item, index) in list" :key="index">
-        <!-- <veneer-nmu :index="index"></veneer-nmu> -->
-        <img v-if="item.status" :src="require(`../../assets/images/veneer/${item.boardname.toLowerCase()}.png`)" alt="" />
+        <component :is="`veneer-${item}`"></component>
+        <!-- <component v-if="item.status && item.boardname" :is="`veneer-${item.boardname.toLowerCase()}`"></component> -->
+        <!-- <img v-if="item.status && item.boardname" :src="require(`../../assets/images/veneer/${item.boardname.toLowerCase()}.png`)" alt="" /> -->
+        <!-- <VeneerM16 v-else></VeneerM16> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import VeneerNmu from "@/components/veneer/nmu";
+import VeneerOlp from "@/components/veneer/olp";
+import VeneerOcp10g from "@/components/veneer/ocp10g";
+import VeneerOtu10g from "@/components/veneer/otu10g";
+import VeneerD16 from "@/components/veneer/d16";
+import VeneerM16 from "@/components/veneer/m16";
+import VeneerEdfa from "@/components/veneer/edfa";
+
 export default {
   name: "structure",
+  components: { VeneerNmu, VeneerOlp, VeneerOcp10g, VeneerOtu10g, VeneerD16, VeneerM16, VeneerEdfa },
   props: {
-    list: {
-      type: Array,
-      default: () => [],
-    },
+    // list: {
+    //   type: Array,
+    //   default: () => [],
+    // },
   },
   data() {
-    return {};
+    return {
+      list: ["nmu", "m16", "olp", "edfa", "d16", "ocp10g", "otu10g"],
+    };
   },
   created() {
     console.log("created");
@@ -32,27 +45,29 @@ export default {
 
 <style scoped lang="scss">
 .structure-wrapper {
-  width: 1000px;
-  height: 236px;
+  width: 1200px;
+  height: 284px;
   background-color: grey;
   background: url("../../assets/images/entirety.jpg") center/contain no-repeat;
   margin: 0 auto;
   position: relative;
 }
+
 .structure-inner-wrapper {
-  width: 868px;
-  height: 200px;
-  // border: 1px solid red;
+  width: 1042px;
+  height: 240px;
   position: absolute;
-  left: 25px;
-  bottom: 0px;
+  left: 32px;
+  bottom: 2px;
   display: flex;
   flex-wrap: wrap;
   flex-direction: row-reverse;
 }
 
 .structure-item-wrapper {
-  height: 50px;
+  height: 60px;
   width: 50% !important;
+  // border: 1px solid #000;
+  // box-sizing: border-box;
 }
 </style>

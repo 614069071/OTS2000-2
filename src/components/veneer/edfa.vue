ebf3218465be9be9576294593dc25c8f9b9d6b19
Jint <614069071@qq.com>
2022-01-18 14:31:38 +0800

    修改edfa olp

--- src/components/veneer/edfa.vue
+++ src/components/veneer/edfa.vue
@@ -1,50 +1,39 @@
 <template>
-  <div class="veneer-olp-view-wrapper veneer-item-view-wrapper">
-    <div class="veneer-item-name"><span class="f-10">OLP</span></div>
-    <div class="veneer-link-wrapper olp-link-wrapper">
+  <div class="veneer-edfa-view-wrapper veneer-item-view-wrapper">
+    <div class="veneer-item-name"><span class="f-10">EDFA</span></div>
+    <div class="veneer-link-wrapper">
       <div class="veneer-light"><span class="k-name">PWR</span><span class="k-item suc"></span><span class="k-item"></span><span class="k-name">Rx1</span></div>
       <div class="veneer-light"><span class="k-name">RUN</span><span class="k-item war"></span><span class="k-item"></span><span class="k-name">Rx2</span></div>
       <div class="veneer-light"><span class="k-name">PROG</span><span class="k-item"></span><span class="k-item"></span><span class="k-name">Rx</span></div>
       <div class="veneer-light"><span class="k-name">Pri</span><span class="k-item"></span><span class="k-item"></span><span class="k-name">Sec</span></div>
     </div>
 
-    <div class="veneer-console-wrapper olp-console-wrapper"><span class="f-9">Console</span></div>
+    <div class="veneer-console-wrapper"><span class="f-9">Console</span></div>
 
-    <div class="veneer-rst-wrapper olp-rst-wrapper">
+    <div class="veneer-rst-wrapper edfa-rst-wrapper">
       <div class="rst-name f-9">RST</div>
       <div class="rst-circle"></div>
     </div>
 
-    <div class="tx-rx-wrapper olp-tx-rx-wrapper">
+    <div class="tx-rx-wrapper edfa-tx-rx-wrapper">
       <div class="tx-rx-inner-item">
         <div class="tx-rx-item">
-          <i class="f-10">tx</i>
+          <i class="f-9">OUT</i>
           <div class="tx-rx-item-main"></div>
         </div>
         <div class="tx-rx-item">
-          <i class="f-10">rx</i>
+          <i class="f-9">IN</i>
           <div class="tx-rx-item-main"></div>
         </div>
       </div>
 
       <div class="tx-rx-inner-item">
         <div class="tx-rx-item">
-          <i class="f-10">tx</i>
+          <i class="f-9">MON</i>
           <div class="tx-rx-item-main"></div>
         </div>
         <div class="tx-rx-item">
-          <i class="f-10">rx</i>
-          <div class="tx-rx-item-main"></div>
-        </div>
-      </div>
-
-      <div class="tx-rx-inner-item">
-        <div class="tx-rx-item">
-          <i class="f-10">tx</i>
-          <div class="tx-rx-item-main"></div>
-        </div>
-        <div class="tx-rx-item">
-          <i class="f-10">rx</i>
+          <i class="f-9">NA</i>
           <div class="tx-rx-item-main"></div>
         </div>
       </div>
@@ -54,7 +43,7 @@
 
 <script>
 export default {
-  name: "veneer-olp",
+  name: "veneer-edfa",
   props: ["data"],
   data() {
     return {};
@@ -69,23 +58,23 @@ export default {
 </script>
 
 <style lang="scss">
-.veneer-olp-view-wrapper {
-  .olp-link-wrapper {
+.veneer-edfa-view-wrapper {
+  .veneer-link-wrapper {
     left: 32px;
     top: 15px;
   }
 
-  .olp-console-wrapper {
+  .veneer-console-wrapper {
     left: 120px;
     bottom: 14px;
   }
 
-  .olp-rst-wrapper {
+  .edfa-rst-wrapper {
     left: 150px;
     top: 12px;
   }
 
-  .olp-tx-rx-wrapper {
+  .edfa-tx-rx-wrapper {
     left: 275px;
     top: 3px;
   }

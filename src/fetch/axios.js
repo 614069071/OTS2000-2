f39f40925778d0165c1e1187cfe9f2798902e330
Jint <614069071@qq.com>
2022-02-11 10:00:00 +0800

    修改超时时间

--- src/fetch/axios.js
+++ src/fetch/axios.js
@@ -1,21 +1,27 @@
 import axios from "axios";
 
 const baseURL = process.env.VUE_APP_BASE_API;
-const serve = axios.create({ baseURL, timeout: 20000 });
+const serve = axios.create({ baseURL, timeout: 5000 });
 
-serve.interceptors.request.use(config => {
-  // config.headers.toKen = store.state.__accessToken__;
-  return config;
-}, err => Promise.reject(err));
+serve.interceptors.request.use(
+  (config) => {
+    // config.headers.toKen = store.state.__accessToken__;
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
 
-serve.interceptors.response.use(response => {
-  if (typeof response.data === 'string') {
-    return Promise.reject(response.data);
+serve.interceptors.response.use(
+  (response) => {
+    if (typeof response.data === "string") {
+      return Promise.reject(response.data);
+    }
+
+    return response.data;
+  },
+  (err) => {
+    return Promise.reject(err);
   }
-
-  return response.data;
-}, err => {
-  return Promise.reject(err);
-});
+);
 
 export default serve;

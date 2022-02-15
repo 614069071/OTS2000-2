7631a430e9638e81bdb6a8de41638454d1951a90
Jint <614069071@qq.com>
2022-02-15 11:22:29 +0800

    合并axios

--- src/fetch/index.js
+++ src/fetch/index.js
@@ -1,7 +1,30 @@
-import axios from './axios';
-import api from './api';
+import axios from "axios";
+
+const baseURL = process.env.VUE_APP_BASE_API;
+const serve = axios.create({ baseURL, timeout: 5000 });
+
+serve.interceptors.request.use(
+  (config) => {
+    // config.headers.toKen = storages.get("__accessToken__");
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
+
+serve.interceptors.response.use(
+  (response) => {
+    if (typeof response.data === "string") {
+      return Promise.reject(response.data);
+    }
+
+    return response.data;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
 
 export default {
-  post: data => axios.post('/action/jsoncdt', data),
-  get: params => axios.post('/action/jsoncdt', { params })
-}
+  post: (data) => serve.post("/action/jsoncdt", data),
+  get: (params) => serve.post("/action/jsoncdt", { params }),
+};

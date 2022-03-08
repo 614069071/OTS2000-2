e62f42f5d5ba9cb66199fb048ec94c09c258e2cb
Jint <614069071@qq.com>
2022-03-08 14:10:22 +0800

    限制发送光功率输入值

--- src/views/equip-manage/board-list/board-view/dco200g.vue
+++ src/views/equip-manage/board-list/board-view/dco200g.vue
@@ -352,7 +352,15 @@
         </tr>
         <tr>
           <td>发送光功率(dBm)</td>
-          <td><input v-if="infoData.CFP2.link_status" type="text" class="def-input" v-model.number="infoData.CFP2.launch_power" /><template v-else>NA</template></td>
+          <td>
+            <input
+              v-if="infoData.CFP2.link_status"
+              type="text"
+              class="def-input"
+              v-limit="{ key: 'infoData.CFP2.launch_power', min: 5, max: 8 }"
+              v-model.number="infoData.CFP2.launch_power"
+            /><template v-else>NA</template>
+          </td>
           <td>接收光功率(dBm)</td>
           <td>{{ infoData.CFP2.link_status ? infoData.CFP2.rcv_power : "NA" }}</td>
           <td></td>

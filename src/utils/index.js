2d0069c7cd85bfe0891e06044ce1af6214372785
Jint <614069071@qq.com>
2022-03-29 15:12:42 +0800

    u

--- src/utils/index.js
+++ src/utils/index.js
@@ -293,8 +293,8 @@ export const mapAlarmTypes = {
   },
   7: {
     name: "200G CFP2 OTU板",
-    lols: [null, "CCH1", "CCH2", "CCH3", "CCH4", "CCH5", "CCH6", "CCH7", "CCH8"],
-    model: [null, "QSFP28-1模块CH1", "QSFP28-1模块CH2", "QSFP28-1模块CH3", "QSFP28-1模块CH4", "QSFP28-2模块CH1", "QSFP28-2模块CH2", "QSFP28-2模块CH3", "QSFP28-2模块CH4"],
+    lols: [null, "CCH1", "CCH2", "CCH3", "CCH4", "CCH5", "CCH6", "CCH7", "CCH8", "CFP2"],
+    model: [null, "QSFP28-1模块CH1", "QSFP28-1模块CH2", "QSFP28-1模块CH3", "QSFP28-1模块CH4", "QSFP28-2模块CH1", "QSFP28-2模块CH2", "QSFP28-2模块CH3", "QSFP28-2模块CH4", "CFP2"],
     other: { 1: "QSFP28-1", 2: "QSFP28-2", 9: "CFP2" },
   },
   8: {
